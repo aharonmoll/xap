00efeb57b07a8e0ff6f3f8d47c897c78241a6c4c
ayelet <ayelet.morris@gigaspaces.com>
2018-11-11 12:34:39 +0200

    Add insightedge platform version

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformVersion.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/version/PlatformVersion.java
@@ -36,14 +36,16 @@ public class PlatformVersion {
     private final int subBuildNumber;
     private final String revision;
     private final String productHelpUrl;
+    private final String product;
 
     public PlatformVersion(Properties properties) {
         version = properties.getProperty("xap.version", "14.0.0");
         milestone = properties.getProperty("xap.milestone", "m1");
         buildNumber = properties.getProperty("xap.build.number", "19901-10");
         revision = properties.getProperty("xap.git.sha", "unspecified");
+        product = properties.getProperty("product.name","XAP2");
 
-        shortOfficialVersion = "XAP " + version + " " + milestone.toUpperCase();
+        shortOfficialVersion = product+" " + version + " " + milestone.toUpperCase();
         officialVersion = "GigaSpaces " + shortOfficialVersion + " (build " + buildNumber + ", revision " + revision + ")";
 
         String[] versionTokens = version.split("\\.");
